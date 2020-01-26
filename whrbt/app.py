59d999c94d3c25a77573d2f2230179141517ab1f
Leon <18955993726@163.com>
2020-01-26 16:44:54 +0800

    customService

--- whrbt/app.py
+++ whrbt/app.py
@@ -1,16 +1,24 @@
 # -*-coding:utf-8-*-
 from werobot import WeRoBot
 import re
+from werobot.client import Client as InformationClient #消息类
+from redis_connect import Connect
 
-from .redis_connect import Connect
-
-client=Connect(host="localhost",port=6379)
-
+r=Connect(host="localhost",port=6379)
+APP_ID=""
+APP_SECRET=""
 class customService:
     """
     客服服务，回复消息，根据city获取用户id，并发送
     """
-    pass
+    def __init__(self,APP_ID,APP_SECRET):
+        config={"APP_ID":APP_ID,"APP_SECRET":APP_SECRET}
+        self.client=InformationClient(config)
+    def addCustomServiceAccount(self,account,nickname,password):
+        self.client.add_custom_service_account(account,nickname,password)
+    def 
+    
+    
 class redisToMysql:
     """
     将redis的内容每过一段时间更新进mysql
@@ -23,10 +31,10 @@ def echo(message):
     sub_city = re.compile("sub(.*)")
     pop_city=re.compile("pop(.*)")
     if sub_city:
-        client.addUser(sub_city,wechat_id)
+        r.addUser(sub_city,wechat_id)
         return "订阅成功"
     if pop_city:
-        client.deleteUser(pop_city,wechat_id)
+        r.deleteUser(pop_city,wechat_id)
         return "删除成功"
     return ""
 
--- whrbt/redis_connect.py
+++ whrbt/redis_connect.py
@@ -1,12 +1,8 @@
 import redis
 class Connect:
-    def __init__(self,host="localhost",port=6379):
-        self.host=host
-        self.port=port
-        self.r=self._connect(self.host,self.port)
-    def _connect(self,host,port):
-        return redis.Redis(host=host,port=port)
-    def addUser(self,wechat_id,city):
-        self.r.sadd(wechat_id,city)
-    def deleteUser(self,wechat_id,city):
-        self.r.
+    def __init__(self,host="localhost",port=6379,password=None):
+        self.r=redis.Redis(host=host,port=port,password=password)
+    def addUser(self,city,wechat_id):
+        self.r.sadd(city,wechat_id)
+    def deleteUser(self,city,wechat_id):
+        self.r.srem(city,wechat_id)
