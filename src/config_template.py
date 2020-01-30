112ca426037a2b5684520bf1419ad34bdf56789e
George Yu <yudumpling@gmail.com>
2020-01-31 02:07:26 +0800

    重新整理文件结构，与个人版本接近统一

---
+++ src/config_template.py
@@ -0,0 +1,21 @@
+# 微信公众号/服务号 token
+TOKEN = 'YOUR_TOKEN'
+TEMPLATE_ID = 'YOUR_TEMPLATE_ID'
+
+# 微信公众号/服务号 AppID 及 AppSecret
+APP_ID = 'YOUR_APP_ID'
+APP_SECRET = 'YOUR_APP_SECRET'
+
+# 机器人监听
+ROBOT_HOST = 'auto' # 默认: 'auto'
+ROBOT_PORT = 8000 # 默认: 8000
+
+# 数据库选择
+DATABASE = 'sqlite' # 目前只支持 sqlite 或 redis
+
+# Redis 服务器
+REDIS_HOST = 'localhost' # 默认: 'localhost'
+REDIS_PORT = 6379 # 默认: 6379
+
+# SQLite 配置
+SQLITE_FILE = 'feiyan-help.sqlite' # 默认: 'feiyan-help.sqlite'
